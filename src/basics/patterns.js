dc5a38ee2c4011bb75b5ef5ccad29edac92c7619
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2024-09-01 15:44:17 +0530

    feat: solve basic pattern problems

--- src/basics/patterns.js
+++ src/basics/patterns.js
@@ -1,4 +1,4 @@
-/** TIme complexity - O(n) Space complexity - O(1) */
+/** Time complexity - O(n) Space complexity - O(1) */
 export function pattern1(n) {
   let str = '';
   let line = '';
@@ -7,6 +7,45 @@ export function pattern1(n) {
   }
   for (let i = 0; i < n; i++) {
     str += line + '\n';
+  }
+  return str;
+}
+
+/** Time complexity - O(n^2) */
+export function pattern2(n) {
+  let str = '';
+
+  for (let i = 0; i < n; i++) {
+    for (let k = 0; k <= i; k++) {
+      str += '* ';
+    }
+    str += '\n';
+  }
+  return str;
+}
+
+/** Time complexity - O(n^2) */
+export function pattern3(n) {
+  let str = '';
+
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= i; k++) {
+      str += `${k}`;
+    }
+    str += '\n';
+  }
+  return str;
+}
+
+/** Time complexity - O(n^2) */
+export function pattern4(n) {
+  let str = '';
+
+  for (let i = 1; i <= n; i++) {
+    for (let k = 1; k <= i; k++) {
+      str += `${i}`;
+    }
+    str += '\n';
   }
   return str;
 }
