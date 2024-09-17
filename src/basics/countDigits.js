8dee8fdaf51f3971735bbd7bfad9d0a3b94a4422
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2024-09-17 20:51:37 +0530

    feat: solve count digits problem optimized

--- src/basics/countDigits.js
+++ src/basics/countDigits.js
@@ -3,8 +3,8 @@
   Space complexity - O(1) 
 */
 export function countDigits(n) {
+  if (n < 0) n = -n;
   if (n === 0) return 1;
-  else if (n < 0) n = -n;
 
   let digits = 0;
   while (n > 0) {
@@ -12,5 +12,17 @@ export function countDigits(n) {
     n = Math.floor(n / 10);
   }
 
+  return digits;
+}
+
+/* 
+  Time complexity - O(1) 
+  Space complexity - O(1) 
+*/
+export function countDigitsOptimized(n) {
+  if (n < 0) n = -n;
+  if (n === 0) return 1;
+
+  const digits = Math.floor(Math.log10(n) + 1);
   return digits;
 }
